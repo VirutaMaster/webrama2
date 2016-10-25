526c3c3122b1fdd2647536fb6f97d00fc8543682
VirutaMaster <virubiru@hotmail.com>
2016-10-25 16:18:48 -0300

    test 9

--- index.php
+++ index.php
@@ -3,39 +3,20 @@
 echo "hello world!";
 require('config/appConfig.php');
 
-/* require('controller/noticia_controller.php');
-require('controller/productos_controller.php');
+require('controller/noticia_controller.php');
+/* require('controller/productos_controller.php');
 require('libs/Smarty.class.php'); */
 
 
-/* class noticia_model
-{
-  private $db;
 
-  public function __construct()
-  {
-    $this->db = new PDO('mysql:host=ec2-54-243-249-137.compute-1.amazonaws.com;dbname=dflqknou6paj1o;charset=utf8','wvqoonnrcxdcxy','B8NquC0rik4ZU8NcxqInf8bPzX');
-
-
-  }
-
-  public function get_noticias(){
-    $select = $this->db->prepare("select * from noticias");
-    $select->execute();
-    $news=$select->fetchAll(PDO::FETCH_ASSOC);
-    print_r($news);
-    return $news;
-  }
-}
- */
  
- $db=new PDO('pgsql:host=ec2-54-243-249-137.compute-1.amazonaws.com;dbname=dflqknou6paj1o','wvqoonnrcxdcxy','B8NquC0rik4ZU8NcxqInf8bPzX');
+/*  $db=new PDO('pgsql:host=ec2-54-243-249-137.compute-1.amazonaws.com;dbname=dflqknou6paj1o','wvqoonnrcxdcxy','B8NquC0rik4ZU8NcxqInf8bPzX');
  print_r($db);
 	$select = $db->prepare("select * from noticias");
     $select->execute();
     $news=$select->fetchAll(PDO::FETCH_ASSOC);
     print_r($news);
-
+ */
 
 /* 
 $productos_controller = new productos_controller();
--- model/noticia_model.php
+++ model/noticia_model.php
@@ -9,13 +9,8 @@ class noticia_model
 
   public function __construct()
   {
-    $this->db = new PDO('mysql:host=localhost;dbname=camping_pesca_motos;charset=utf8','usuario1','123456');
-    // $this->news = array (
-    //   array("101","La noticia 1","Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
-    //   array("102","La noticia 2","Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
-    //   array("103","La noticia 3","Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
-    //   array("104","La noticia 4","Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
-    // );
+    $this->db = new PDO('pgsql:host=ec2-54-243-249-137.compute-1.amazonaws.com;dbname=dflqknou6paj1o','wvqoonnrcxdcxy','B8NquC0rik4ZU8NcxqInf8bPzX');
+
 
   }
 
